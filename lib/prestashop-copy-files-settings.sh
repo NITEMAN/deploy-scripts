ccc054e16aacf9ce52d715ec6f2236d71d648ae8
Jonathan Araña Cruz <jonhattan@faita.net>
2013-12-07 14:10:53 +0000

    Prestashop autodeploy.

---
+++ lib/prestashop-copy-files-settings.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# Type of script: unattended
+# To be runned on: -
+# To be runned by: root
+# Arguments: $SOURCE $TARGET
+# Local dependencies: - none -
+# Remote dependencies: - none -
+# Short description: A script to copy files (and non existant settings.php) from $SOURCE to TARGET Prestashop installations
+
+E_BADARGS=65
+E_BADDEPS=66
+E_SOURCE_DONT_EXISTS=67
+E_TARGET_DONT_EXISTS=68
+
+EXPECTED_ARGS=2
+
+#echo $0
+if [ $# -ne $EXPECTED_ARGS ]
+then
+  echo " This script expect $EXPECTED_ARGS arguments instead of $#"
+  exit $E_BADARGS
+fi
+
+AWK=$(which awk)
+MKDIR=$(which mkdir)
+RM=$(which rm)
+CP=$(which cp)
+
+SOURCE=$1
+TARGET=$2
+echo "*** Copying files & settings from $SOURCE $TARGET"
+
+if [ ! -d $SOURCE ]; then
+  echo " ABORTED: Source $SOURCE doesn't exists"
+  exit $E_SOURCE_DONT_EXISTS
+fi
+if [ ! -d $TARGET ]; then
+  echo " ABORTED: Target $TARGET doesn't exists"
+  exit $E_TARGET_DONT_EXISTS
+fi
+
